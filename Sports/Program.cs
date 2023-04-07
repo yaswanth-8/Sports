f12ee709d8388a2c6b9cd4d73ec27855eec9e3dc
yaswanth <ya8wanth@gmail.com>
2023-04-07 11:26:31 +0530

    Display sports

--- Sports/Program.cs
+++ Sports/Program.cs
@@ -5,8 +5,27 @@ using System.Data;
 namespace MyProject;
 class Program
 {
+    public static void displaySports()
+    {
+        string connectionString = "Data Source=DESKTOP-A1NJHOG;Initial Catalog=Sports;Integrated Security=True;Encrypt=False;";
+        using (SqlConnection connection = new SqlConnection(connectionString))
+        {
+            connection.Open();
+            string query = "SELECT * FROM Sports";
+            using (SqlCommand command = new SqlCommand(query, connection))
+            {
+                using (SqlDataReader reader = command.ExecuteReader())
+                {
+                    while (reader.Read())
+                    {
+                        Console.WriteLine(reader[0] + "   " + reader[1]);
+                    }
+                }
+            }
+        }
+    }
     static void Main(string[] args)
     {
-       
+        displaySports();
     }
 }
